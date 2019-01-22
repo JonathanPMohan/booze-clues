599eb3bb7b03164bf0d92035b6580a0292df6357
Jonathan Mohan <JonathanMohan.net@gmail.com>
2019-01-21 21:22:33 -0600

    nav bar up/routing working

---
+++ src/components/pages/EditClue/EditClue.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './EditClue.scss';
+
+class EditClue extends React.Component {
+  render() {
+    return (
+      <div className='editClue'>
+        <h2>Edit That Clue!</h2>
+      </div>
+    );
+  }
+}
+
+export default EditClue;
---
+++ src/components/pages/NewClue/NewClue.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './NewClue.scss';
+
+class NewClue extends React.Component {
+  render() {
+    return (
+      <div className='newClue'>
+        <h2>Gimmie That Clue!</h2>
+      </div>
+    );
+  }
+}
+
+export default NewClue;

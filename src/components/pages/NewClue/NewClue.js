5a39b3f148575ed6e02ab9a0ba4c624154967416
Jonathan Mohan <JonathanMohan.net@gmail.com>
2019-01-22 21:32:35 -0600

    clue form built

--- src/components/pages/NewClue/NewClue.js
+++ src/components/pages/NewClue/NewClue.js
@@ -1,11 +1,135 @@
 import React from 'react';
+import { Button } from 'reactstrap';
+import PropTypes from 'prop-types';
+import authRequests from '../../../helpers/data/authRequests';
 import './NewClue.scss';
 
+const defaultClue = {
+  name: '',
+  imageUrl: '',
+  location: '',
+  style: '',
+  notes: '',
+  uid: '',
+};
+
 class NewClue extends React.Component {
+  static propTypes = {
+    onSubmit: PropTypes.func,
+  }
+
+  state = {
+    newClue: defaultClue,
+  }
+
+  formFieldStringState = (name, e) => {
+    e.preventDefault();
+    const tempClue = { ...this.state.newClue };
+    tempClue[name] = e.target.value;
+    this.setState({ newClue: tempClue });
+  }
+
+  nameChange = e => this.formFieldStringState('name', e);
+
+  locationChange = e => this.formFieldStringState('location', e);
+
+  imageChange = e => this.formFieldStringState('imageUrl', e);
+
+  styleChange = e => this.formFieldStringState('style', e);
+
+  notesChange = e => this.formFieldStringState('notes', e);
+
+  formSubmit = (e) => {
+    e.preventDefault();
+    const { onSubmit } = this.props;
+    const myClue = { ...this.state.newClue };
+    myClue.uid = authRequests.getCurrentUid();
+    onSubmit(myClue);
+    this.setState({ newClue: defaultClue });
+  }
+
   render() {
+    const { newClue } = this.state;
+
     return (
-      <div className='newClue'>
-        <h2>Gimmie That Clue!</h2>
+      <div className='newClue mx-auto'>
+        <div className="clue-form mt-5">
+          <form onSubmit={this.formSubmit}>
+            <div className="input-group mb-3">
+              <div className="input-group-prepend">
+                <span className="input-group-text" id="name-pre">Name</span>
+              </div>
+              <input
+                type="text"
+                id="name"
+                className="form-control"
+                placeholder="Clue Name"
+                aria-describedby="nameHelp"
+                value={newClue.name}
+                onChange={this.nameChange}
+              />
+            </div>
+            <div className="input-group mb-3">
+              <div className="input-group-prepend">
+                <span className="input-group-text" id="imageUrl-pre">Image URL</span>
+              </div>
+              <input
+                type="text"
+                id="imageUrl"
+                className="form-control"
+                placeholder="Clue Image"
+                aria-describedby="imageHelp"
+                value={newClue.imageUrl}
+                onChange={this.imageUrlChange}
+              />
+            </div>
+            <div className="input-group mb-3">
+              <div className="input-group-prepend">
+                <span className="input-group-text" id="location-pre">Location</span>
+              </div>
+              <input
+                type="text"
+                id="location"
+                className="form-control"
+                placeholder="Clue Location"
+                aria-describedby="locationHelp"
+                value={newClue.location}
+                onChange={this.locationChange}
+              />
+            </div>
+            <div className="input-group mb-3">
+              <div className="input-group-prepend">
+                <span className="input-group-text" id="style-pre">Style</span>
+              </div>
+              <input
+                type="text"
+                id="style"
+                className="form-control"
+                placeholder="Clue Style"
+                aria-describedby="styleHelp"
+                value={newClue.style}
+                onChange={this.styleChange}
+              />
+            </div>
+            <div className="input-group mb-3">
+              <div className="input-group-prepend">
+                <span className="input-group-text" id="notes-pre">Notes</span>
+              </div>
+              <input
+                type="text"
+                id="notes"
+                className="form-control"
+                placeholder="Clue Notes"
+                aria-describedby="notesHelp"
+                value={newClue.notes}
+                onChange={this.notesChange}
+              />
+            </div>
+            <Button className="btn btn-secondary mt-3" onSubmit={this.formSubmit}>
+              Submit New Clue
+            </Button>
+          </form>
+        </div>
       </div>
     );
   }

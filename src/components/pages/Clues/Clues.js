54ba772cb82dac326f56d304dad35d931c2fa648
Jonathan Mohan <JonathanMohan.net@gmail.com>
2019-01-28 21:07:22 -0600

    got clues filtering

--- src/components/pages/Clues/Clues.js
+++ src/components/pages/Clues/Clues.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Button } from 'reactstrap';
+import SearchField from 'react-search-field';
 import cluesData from '../../../helpers/data/cluesData';
 import authRequests from '../../../helpers/data/authRequests';
 import PrintClueCard from '../../PrintClueCard/PrintClueCard';
@@ -9,6 +10,7 @@ import './Clues.scss';
 class Clues extends React.Component {
   state = {
     clues: [],
+    filteredClues: [],
   }
 
   getClues = () => {
@@ -16,6 +18,7 @@ class Clues extends React.Component {
     cluesData.getAllClues(uid)
       .then((clues) => {
         this.setState({ clues });
+        this.setState({ filteredClues: clues });
       })
       .catch((err) => {
         console.error('error with clues GET', err);
@@ -43,8 +46,30 @@ class Clues extends React.Component {
     this.props.history.push(`/clues/${clueId}/edit`);
   }
 
+  onChange = (value, event) => {
+    const { clues } = this.state;
+    const filteredClues = [];
+    event.preventDefault();
+    if (!value) {
+      this.setState({ filteredClues: clues });
+    } else {
+      clues.forEach((clue) => {
+        if (clue.name.toLowerCase().includes(value.toLowerCase())
+          || clue.style.toLowerCase().includes(value.toLowerCase())
+          || clue.location.toLowerCase().includes(value.toLowerCase())) {
+          filteredClues.push(clue);
+        }
+        this.setState({ filteredClues });
+      });
+    }
+  }
+
   render() {
-    const printClue = this.state.clues.map(clue => (
+    const {
+      filteredClues,
+    } = this.state;
+
+    const printClue = filteredClues.map(clue => (
       <PrintClueCard
         key={clue.id}
         clue={clue}
@@ -56,6 +81,12 @@ class Clues extends React.Component {
     return (
       <div className='clues mx-auto'>
         <h2>Searching For A Clue?</h2>
+        <SearchField
+          placeholder="Search Clues By Name or Style"
+          onChange={this.onChange}
+          searchText=""
+          classNames="test-class w-100"
+        />
         <Button className="btn btn-info mt-5" id="addClue" onClick={this.newClueView}>Add Clue</Button>
         <div className="row justify-content-center">{printClue}</div>
       </div>

d7b45fd7fa09280c3c59a964c6f99b52e3306645
Jonathan Mohan <JonathanMohan.net@gmail.com>
2019-02-18 01:59:43 -0600

    more

--- src/components/pages/Home/Home.js
+++ src/components/pages/Home/Home.js
@@ -13,6 +13,7 @@ class Home extends React.Component {
   render() {
     return (
       <div className='home mx-auto animated bounceInLeft'>
+        <h2>Find A Clue, Location, or Browse Your Collection</h2>
         <div className="deck card-deck mt-5">
           <div className="card border-dark" id="clues" onClick={this.changeView}>
             <div className="card-body text-center">
